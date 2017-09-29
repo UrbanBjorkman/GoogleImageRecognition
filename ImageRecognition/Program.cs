58dbca5c03954d5bbc79924ec002a39e92d3683c
Urban Björkman <urban.bjorkman@gmail.com>
2017-09-29 08:41:41 +0200

    Some fixes and comments

--- ImageRecognition/Program.cs
+++ ImageRecognition/Program.cs
@@ -1,17 +1,13 @@
 ﻿using Google.Apis.Auth.OAuth2;
-using Google.Cloud.Storage.V1;
 using Google.Cloud.Vision.V1;
 using Grpc.Auth;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ImageRecognition
 {
-    class Program
+	class Program
     {
 
         public const string credentialFile = "ImageRecognition-0d0e795f6b75.json";
@@ -19,16 +15,22 @@ namespace ImageRecognition
 
         static void Main(string[] args)
         {
-			System.IO.Directory.CreateDirectory(@"c:\image\");
+			// Create directory
+			Directory.CreateDirectory(@"c:\image\");
 
-			FileSystemWatcher watcher = new FileSystemWatcher();
-            watcher.Path = @"c:\image\";
-            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName;
-            watcher.Filter = "*.*";
-            watcher.Created += new FileSystemEventHandler(FileAdded);
+
+			// Setup watcher for image directory
+			FileSystemWatcher watcher = new FileSystemWatcher()
+			{
+				Path = @"c:\image\",
+				NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName,
+				Filter = "*.*"
+			};
+			watcher.Created += new FileSystemEventHandler(FileAdded);
             watcher.EnableRaisingEvents = true;
 
 
+			// Keep looping until escapekey
             while (Console.ReadKey().Key != ConsoleKey.Escape) { }
 
         }
@@ -61,15 +63,24 @@ namespace ImageRecognition
                     Console.WriteLine(string.Format("{0:0.00}", annotation.Score) + "% " + annotation.Description);
             }
             Console.WriteLine(" *****************");
-            Console.WriteLine(" ");
-            Console.WriteLine(" *** Texts ***");
+            
 
+			//Textdetection
 			var textResponse = client.DetectText(image);
-			foreach (var annotation in textResponse)
+
+			if (textResponse.Any())
 			{
-				if (annotation.Description != null)
-					Console.WriteLine(annotation.Description);
+				Console.WriteLine(" ");
+				Console.WriteLine(" *** Texts ***");
+				foreach (var annotation in textResponse)
+				{
+					if (annotation.Description != null)
+						Console.WriteLine(annotation.Description);
+				}
+				Console.WriteLine(" *****************");
 			}
+			
+			Console.WriteLine(" ");
 		}
 
 
